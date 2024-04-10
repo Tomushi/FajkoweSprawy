9bd28c7dcc330a359444ceeb4a07981f9bbd4af6
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-04-10 23:51:10 +0200

    Bedzie kolejny, wewnetrzny switch

--- FajkoweSprawy/Program.cs
+++ FajkoweSprawy/Program.cs
@@ -1,5 +1,27 @@
 ﻿// See https:/// Coś tu trzeba wymyślić ...
 
+Magazyn maga = new Magazyn();
+
+bool czyKontynuowac = true;
 
 Console.WriteLine("Tytoń i jego rodzaje");
-Console.WriteLine("Czas na fajkę");
+
+while (czyKontynuowac)
+{
+    Console.WriteLine("Wybierz operacje:");
+    Console.WriteLine("1. Dodaj tyton");
+    Console.WriteLine("2. Wyswietl tytonie");
+    Console.WriteLine("3. Zamknij program");
+    Console.WriteLine("Wpisz numer operacji i potwierdz klawisze Enter");
+
+    string operacja = Console.ReadLine();
+
+    switch (operacja)
+    {
+        case "1":
+            Console.WriteLine("Wpisz nazwe i wcisnij Enter");
+            string nazwa = Console.ReadLine();
+            Console.WriteLine("Wbierz moc tytoniu");
+            string moc = Console.ReadLine();
+    }
+}
