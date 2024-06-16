b9934faa9c7940e4d2eab6fb9405341be4788d01
Tomek <107033433+Tomushi@users.noreply.github.com>
2024-06-16 16:35:54 +0200

    Update Tyton.cs

--- FajkoweSprawy/Magazyn/Tyton.cs
+++ FajkoweSprawy/Magazyn/Tyton.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,7 @@ namespace FajkoweSprawy.Magazyn
 
         public void WyswietlInformacjeOTytoniu()
         {
-            Console.WriteLine($"Nazwa: {Nazwa}");
+            Console.WriteLine($"nazwa: {Nazwa}, moc: {Moc}");
         }
     }
 }
